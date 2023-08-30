d4b1eb4f6b6d48bd978bc903b1083b9e5881fcd4
Abdelrahman Zaitoun <78018369+abdelrahman830a@users.noreply.github.com>
2023-08-30 12:08:54 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./search.svg";
 
-const API_URL = "http://www.omdbapi.com?apikey=ff6d82fc";
+const API_URL = "https://www.omdbapi.com?apikey=ff6d82fc";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
